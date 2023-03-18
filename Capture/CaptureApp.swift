59326c19ca159ada45eea9874fb1d3b484508299
Kevin <kevin@kvnd.me>
2023-03-18 16:52:46 -0500

    Initial Commit

---
+++ Capture/CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureApp.swift
+//  Capture
+//
+//  Created by Kevin Donnelly on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
